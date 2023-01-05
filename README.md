92e319f018959f10114cde401da1803e31143b05
Timo <smartie8@hotmail.de>
2023-01-05 11:49:14 +0100

    Fixed some errors, recursive directory checking, adapting local folder structure to remote folder structure, added recurring backups

---
+++ README.md
@@ -0,0 +1,21 @@
+# FTPBackup
+
+Just another FTP Backup client for backing up files inside a remote directory.
+Avoids duplicate backups by comparing with local backups.
+
+## Deployment
+
+To deploy this project run
+
+```bash
+  npm i
+  npm start
+```
+
+Copy the .env.example to .env with
+
+```bash
+  cp .env.example .env
+```
+
+and fill the necessary information.
--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 require("dotenv").config();
+require("./lib/FTPService")
+const cron = require('node-cron');
 const triggerBackup = require("./lib/FTPService");
+const writeLog = require("./lib/LogService");
+const { envVal } = require('env-bool');
 
-triggerBackup();
+
+(() => {
+    if(envVal(process.env.CRON_ENABLED)){
+        writeLog("Recurring backups enabled.");
+        if(envVal(process.env.RUN_EVERY_DAY)){
+            cron.schedule("0 1 * * *", () => {
+                console.log("sad")
+                triggerBackup();
+            });
+        } else if(envVal(process.env.RUN_EVERY_WEEK)){
+            cron.schedule("0 1 * * SUN", () => {
+                console.log("asdasd")
+                triggerBackup();
+            });
+        }
+
+    } else {
+        writeLog("Recurring backups disabled, so executing one time and then aborting.")
+    }
+})();
